4008beb91a4f729752bca7fdbccf0dca0bdbea95
Adam Freemer <a@freemer.com>
2015-01-06 00:01:45 -0500

    Update for unix sockets.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,27 +1,31 @@
-
 application = 'overheard'
 environment =  ENV['RAILS_ENV'] || 'production'
 app_path =  "/var/www/overheardby.me"
 
+
+timeout 30
+preload_app true
+worker_processes 6
+
+GC.respond_to?(:copy_on_write_friendly=) and GC.copy_on_write_friendly = true
+
+
+
+
+listen "/tmp/unicorn.overheardbyme.sock"
+# listen 2000
+
+
 working_directory "/var/www/overheardby.me/current"
 pid "/var/www/overheardby.me/shared/pids/unicorn.pid"
 
-# Hard-set the CWD & pidfile to ensure app-reloading consistency
-if %w(production staging).include?(environment)
-  working_directory "#{app_path}/current"
-  shared_path = "#{app_path}/shared"
-else
-  working_directory app_path
-  shared_path = app_path
-end
-
-listen 3000
-timeout 30
-preload_app true
-worker_processes 2
-GC.respond_to?(:copy_on_write_friendly=) and GC.copy_on_write_friendly = true
 
 stderr_path "#{shared_path}/log/unicorn.stderr.log"
+
+
+
+
+
 
 # Rails breaks unicorn's logger formatting, reset it
 # http://rubyforge.org/pipermail/mongrel-unicorn/2010-October/000732.html
@@ -37,3 +41,11 @@ end
 
 
 
+# Hard-set the CWD & pidfile to ensure app-reloading consistency
+# if %w(production staging).include?(environment)
+#   working_directory "#{app_path}/current"
+#   shared_path = "#{app_path}/shared"
+# else
+#   working_directory app_path
+#   shared_path = app_path
+# end
